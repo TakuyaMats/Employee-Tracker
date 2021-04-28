1e06b802a0198ebf18833d1d98014b2441337640
TMats <takomats@hotmail.com>
2021-04-28 14:47:06 -0400

    viewAll() and viewAllDeparment() function completed, addEmployee() function still needs refractoring.

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
-const consoleTable = require('console.table');
+const cTable = require('console.table');
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
@@ -43,32 +43,273 @@ const start = () => {
             ],
         })
         .then((answer) => {
-            if (answer.employeeManager === 'View All Employees') {
-                viewAll();
-            } else if (answer.employeeManager === 'View All Employees By Department') {
-                viewAllDepartment();
-            } else if (answer.employeeManager === 'View All Employees By Manager') {
-                viewAllManager();
-            } else if (answer.employeeManager === 'Add Employee') {
-                addEmployee();
-            } else if (answer.employeeManager === 'Remove Employee') {
-                removeEmployee();
-            } else if (answer.employeeManager === 'Update Employee Role') {
-                updateEmployeeRole();
-            } else if (answer.employeeManager === 'Update Employee Manager') {
-                updateEmployeeManager();
-            } else if (answer.employeeManager === 'View All Roles') {
-                viewAllRoles();
-            } else if (answer.employeeManager === 'Add Role') {
-                addRoles();
-            } else if (answer.employeeManager === 'Remove Role') {
-                removeRoles();
-            } else {
-                connection.end();
+            switch (answer.employeeManager) {
+                case "View All Employees":
+                    viewAll()
+                    break;
+                case "View All Employees By Department":
+                    viewAllDepartment()
+                    break;
+                case "View All Employees By Manager":
+                    viewAllManager()
+                    break;
+                case "Add Employee":
+                    addEmployee()
+                    break;
+                case "Remove Employee":
+                    removeEmployee()
+                    break;
+                case "Update Employee Role":
+                    updateEmployeeRole()
+                    break;
+                case "Update Employee Manager":
+                    updateEmployeeManager()
+                    break;
+                case "View All Roles":
+                    viewAllRoles()
+                    break;
+                case "Add Role":
+                    addRoles()
+                    break;
+                case "Remove Role":
+                    removeRoles()
+                    break;
+                case "Exit":
+                    connection.end()
+                    break;
             }
         });
 };
 
+const viewAll = () => {
+    console.log("------------------------------------------");
+    connection.query(`SELECT 
+    e.id,
+    CONCAT(e.first_name, ' ', e.last_name) AS 'Employee Name',
+    r.title,
+    d.name AS 'Department',
+    r.salary,
+    CONCAT(m.first_name, ' ', m.last_name) AS 'Manager'
+    FROM employee e
+    LEFT JOIN employee m
+    ON e.manager_id = m.role_id
+    JOIN roles r
+    ON e.role_id = r.id
+    JOIN department d
+    ON r.department_id = d.id;`, (err, res) => {
+        if (err) throw err;
+        let data = res
+        console.table(data);
+        inquirer.prompt([{
+            type: "list",
+            name: "choices",
+            message: "Would you like to go back to the main menu or exit?",
+            choices: [
+                "Main Menu",
+                "Exit",
+            ]
+        }]).then(data => {
+            switch (data.choices) {
+                case "Main Menu":
+                    start();
+                    break;
+                case "Exit":
+                    connection.end();
+                    break;
+            }
+        })
+    })
+};
+
+const viewAllDepartment = () => {
+    console.log("------------------------------------------");
+    connection.query(`SELECT 
+    e.id,
+    CONCAT(e.first_name, ' ', e.last_name) AS 'Employee Name',
+    r.title,
+    d.name AS 'Department'
+    FROM employee e
+    LEFT JOIN employee m
+    ON e.manager_id = m.role_id
+    JOIN roles r
+    ON e.role_id = r.id
+    JOIN department d
+    ON r.department_id = d.id;`, (err, res) => {
+        if (err) throw err;
+        let data = res
+        console.table(data);
+        inquirer.prompt([{
+            type: "list",
+            name: "choices",
+            message: "Would you like to go back to the main menu or exit?",
+            choices: [
+                "Main Menu",
+                "Exit",
+            ]
+        }]).then(data => {
+            switch (data.choices) {
+                case "Main Menu":
+                    start();
+                    break;
+                case "Exit":
+                    connection.end();
+                    break;
+            }
+        })
+    })
+};
+
+const viewAllManager = () => {
+    console.log("------------------------------------------");
+    connection.query(`SELECT 
+    e.id,
+    CONCAT(e.first_name, ' ', e.last_name) AS 'Employee Name',
+    CONCAT(m.first_name, ' ', m.last_name) AS 'Manager'
+    FROM employee e
+    LEFT JOIN employee m
+    ON e.manager_id = m.role_id
+    JOIN roles r
+    ON e.role_id = r.id
+    JOIN department d
+    ON r.department_id = d.id;`, (err, res) => {
+        if (err) throw err;
+        let data = res
+        console.table(data);
+        inquirer.prompt([{
+            type: "list",
+            name: "choices",
+            message: "Would you like to go back to the main menu or exit?",
+            choices: [
+                "Main Menu",
+                "Exit",
+            ]
+        }]).then(data => {
+            switch (data.choices) {
+                case "Main Menu":
+                    start();
+                    break;
+                case "Exit":
+                    connection.end();
+                    break;
+            }
+        })
+    })
+};
+
+const addEmployee = () => {
+    inquirer
+        .prompt([{
+                name: 'firstName',
+                type: 'input',
+                message: "What is the employee's first name?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    } else {
+                        return "Please enter at least one Character.";
+                    }
+                }
+            },
+            {
+                name: 'lastName',
+                type: 'input',
+                message: "What is the employee's last name?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    } else {
+                        return "Please enter at least one Character.";
+                    }
+                }
+            },
+            {
+                name: 'role',
+                type: 'choices',
+                message: "What is the employee's role?",
+                choices: [
+                    "Lead Engineer",
+                    "Legal Team Lead",
+                    "Accountant",
+                    "Sales Lead",
+                    "Salesperson",
+                    "Software Engineer",
+                    "Lawyer",
+                    "Compensation",
+                    "Employee Relations",
+                    "Marketing Strategist Lead",
+                    "Social Media Lead",
+                    "Customer Support",
+                ]
+            },
+            {
+                name: 'manager',
+                message: "Who is the employee's manager?",
+                choices: [
+                    "John Doe",
+                    "Mike Hunt",
+                    "Cathy Beach"
+                ]
+            }
+        ]).then((answer) => {
+            connection.query(
+                'INSERT INTO employee SET ?',
+                {
+                    first_name: answer.firstName,
+                    last_name: answer.lastName,
+                    role_id: answer.role,
+                    manager_id: answer.manager,
+                },
+                
+                (err, res) => {
+                    if (err) throw err;
+                    let data = res
+                    console.table(data);
+                    inquirer.prompt([{
+                        type: "list",
+                        name: "choices",
+                        message: "Would you like to go back to the main menu or exit?",
+                        choices: [
+                            "Main Menu",
+                            "Exit",
+                        ]
+                    }]).then(data => {
+                        switch (data.choices) {
+                            case "Main Menu":
+                                start();
+                                break;
+                            case "Exit":
+                                connection.end();
+                                break;
+                        }
+                    })
+                }
+            )
+        })
+};
+
+const removeEmployee = () => {
+
+};
+
+const updateEmployeeRole = () => {
+
+};
+
+const updateEmployeeManager = () => {
+
+};
+
+const viewAllRoles = () => {
+
+};
+
+const addRoles = () => {
+
+};
+
+const removeRoles = () => {
+
+};
 
 
 connection.connect((err) => {
